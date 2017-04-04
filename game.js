b9e3ba82b5cd41a0baaec6ed080098ca7c3984d0
maliklau <malikl0799@gmail.com>
2017-04-04 17:58:57 -0400

    fixed isNan to isNaNgit add . and else locaiton conditional

--- game.js
+++ game.js
@@ -1,3 +1,6 @@
+var companions
+var locationchosen
+var randInt
 var chosenName = window.prompt('What character name do you want?')
 if (chosenName === null || chosenName === undefined || chosenName === ' ' || chosenName === '') {
   chosenName = 'NoName'
@@ -6,98 +9,88 @@ chosenName = chosenName.trim().toLowerCase()
 var decideToKeep = window.confirm('Are you sure you want to continue into this exciting game, ' + chosenName + ' ?')
 if (decideToKeep === false) {
   window.alert('Good Bye, you missed out!')
-}
-else {
+} else {
   var stringDis = 'Welcome to the adventure game, ' + chosenName
   window.alert(stringDis)
-  var location = window.prompt('Where do you want your adventure to start...Saturn, the rainforest, or the underworld?')
-  if (location === null || location === undefined || location === ' ' || location === '') {
-    chosenName = 'earth'
+  locationchosen = window.prompt('Where do you want your adventure to start...Saturn, the rainforest, or the underworld?')
+  if (locationchosen === null || locationchosen === undefined || locationchosen === ' ' || locationchosen === '') {
+    locationchosen = 'earth'
   }
-  location = chosenName.trim().toLowerCase()
-  if (location === 'saturn') {
+  locationchosen = locationchosen.trim().toLowerCase()
+  if (locationchosen === 'saturn') {
     window.alert('Dressed in your space suit you must navigate the gas giant planet to find the rare galactic geode.')
-    var companions = window.prompt('How many companions do you want with you?')
+    companions = window.prompt('How many companions do you want with you?')
     if (companions === null || companions === undefined || companions === ' ' || companions === '') {
       companions = '2'
     }
-    var compInt = parseInt(companions)
-    if (isNan(companions)) {
-      compInt = 2
+    companions = parseInt(companions)
+    if (isNaN(companions)) {
+      companions = 2
     }
-    var randInt = parseInt(Math.random())
-    if (compInt > randInt) {
+    randInt = parseInt(Math.random())
+    if (companions > randInt) {
       window.alert('Yay you beat the aliens to the geode')
-    }
-    else if (compInt < randInt){
+    } else if (companions < randInt) {
       window.alert('Unfortunately you did not get the geode.')
-    }
-    else {
+    } else if (isNaN(companions)) {
+      window.alert('NOT A NUMBER! OOPS :(')
+    } else {
       window.alert('You decide to eat instead of continue to look for the geode.')
     }
-  }
-  else if (location === 'the rainforest') {
+  } else if (locationchosen === 'the rainforest') {
     window.alert('You must travle the rainforest in search of the mighty orangatang oracle to get answers about life.')
-    var companions = window.prompt('How many companions do you want with you?')
+    companions = window.prompt('How many companions do you want with you?')
     if (companions === null || companions === undefined || companions === ' ' || companions === '') {
       companions = '2'
     }
-    var compInt = parseInt(companions)
-    if (isNan(companions)) {
-      compInt = 2
+    companions = parseInt(companions)
+    if (isNaN(companions)) {
+      companions = 2
     }
-    var randInt = parseInt(Math.random())
-    if (compInt > randInt) {
+    randInt = parseInt(Math.random())
+    if (companions > randInt) {
       window.alert('You have found the oracle and will live a prosperous life.')
-    }
-    else if (compInt < randInt){
+    } else if (companions < randInt) {
       window.alert('Unfortunately you did not get to the orangatang oracle and had to fight a python instead.')
-    }
-    else {
+    } else if (isNaN(companions)) {
+      window.alert('NOT A NUMBER! OOPS :(')
+    } else {
       window.alert('You decide to eat instead of search for answers to life.')
     }
-  }
-  else if (location === 'the underworld') {
+  } else if (locationchosen === 'the underworld') {
     window.alert('Unfortunately you are stuck here until you find your own way out through the magic portal of white light')
-    var companions = window.prompt('How many companions do you want with you?')
+    companions = window.prompt('How many companions do you want with you?')
     if (companions === null || companions === undefined || companions === ' ' || companions === '') {
-      companions = '2'
+      companions = 2
     }
-    var compInt = parseInt(companions)
-    if (isNan(companions)) {
-      compInt = 2
-    }
-    var randInt = parseInt(Math.random())
-    if (compInt > randInt) {
+    companions = parseInt(companions)
+    randInt = parseInt(Math.random() * 3)
+    if (companions > randInt) {
       window.alert('Yay, you discovered the exit of the underworld!')
-    }
-    else if (compInt < randInt){
-      window.alert('Unfortunately you are wondering around in circles.')
-    }
-    else {
+    } else if (companions < randInt) {
+      window.alert('Unfortunately you are wandering around in circles.')
+    } else if (isNaN(companions)) {
+      window.alert('NOT A NUMBER! OOPS :(')
+    } else {
       window.alert('You decide to eat instead of continue to find a way out.')
     }
-  }
-  else {
-    location = 'earth'
+  } else {
+    locationchosen = 'earth'
     window.alert('Did you try to create your own location? Well, now you get earth by default.')
     window.alert('You must save all the polar bears and people of Florida before global warming drowns them and their habitats.')
-    var companions = window.prompt('How many companions do you want with you?')
+    companions = window.prompt('How many companions do you want with you?')
     if (companions === null || companions === undefined || companions === ' ' || companions === '') {
       companions = '2'
     }
-    var compInt = parseInt(companions)
-    if (isNan(companions)) {
-      compInt = 2
-    }
-    var randInt = Math.random()
-    if (compInt > randInt) {
+    companions = parseInt(companions)
+    randInt = Math.random()
+    if (companions > randInt) {
       window.alert('Yes! Your grandchild will know what Dinsney World is and what polar bears look like!')
-    }
-    else if (compInt < randInt){
+    } else if (companions < randInt) {
       window.alert('Unfortunately, you failed.')
-    }
-    else {
+    } else if (isNaN(companions)) {
+      window.alert('NOT A NUMBER! OOPS :(')
+    } else {
       window.alert('You decide to eat instead of continue to stop global warming.')
     }
   }
